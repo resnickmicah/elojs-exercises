5c5893e06d9471c0c71930263a190dbdea947654
Micah Resnick <resnickmicah@gmail.com>
2024-05-31 15:50:20 -0700

    Fix ch6ex2

--- ch6-secret-life-of-objs/ex2-group-set.js
+++ ch6-secret-life-of-objs/ex2-group-set.js
@@ -4,28 +4,31 @@ class Group {
 
     //  Its constructor creates an empty group
     constructor() {
-        this.values = {};
+        this.values = [];
     }
 
     // add adds a value to the group (but only if it isn’t already a member)
     add(val) {
-        this.values[val] = true;
+        if (this.values.indexOf(val) == -1) {
+            this.values.push(val);
+        }
     }
 
     // delete removes its argument from the group (if it was a member)
     delete(val) {
-        if (this.values[val]) {
-            delete this.values[val];
+        let idx = this.values.indexOf(val);
+        if (idx != -1) {
+            this.values.splice(idx, 1);
         }
     }
 
     // has returns a Boolean value indicating whether its argument is a member of the group.
     has(val) {
-        return this.values[val] || false;
+        return this.values.indexOf(val) != -1;
     }
 
     toString() {
-        return Object.keys(this.values).toString();
+        return this.values.toString();
     }
 
     // Give the class a static from method that takes an iterable object as argument and creates a group that contains all the values produced by iterating over it.
@@ -42,14 +45,14 @@ class Group {
 
 let group = Group.from([10, 20]);
 console.log(`Initialized group with 10, 20: ${group}`);
-console.log(`Group has 10: ${group.has(10)}`);
+console.log(`Group has 10 (should return true): ${group.has(10)}`);
 // → true
 console.log();
-console.log(`Group has 30: ${group.has(30)}`);
+console.log(`Group has 30 (should return false): ${group.has(30)}`);
 // → false
 group.add(10);
 console.log(`Group still has 10, 20 after adding 10: ${group}`);
 group.delete(10);
 console.log(`Group only has 20 after deleting 10: ${group}`);
-console.log(`Group no longer has 10: ${group.has(10)}`);
+console.log(`Group no longer has 10 (should return false): ${group.has(10)}`);
 // → false
