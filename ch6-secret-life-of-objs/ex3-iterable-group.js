00c3f595a6b8f58bda36c911887703288430b6b4
Micah Resnick <resnickmicah@gmail.com>
2024-05-31 17:18:42 -0700

    ch6ex3 move iterator init to group constructor

--- ch6-secret-life-of-objs/ex3-iterable-group.js
+++ ch6-secret-life-of-objs/ex3-iterable-group.js
@@ -7,6 +7,9 @@ class Group {
     constructor() {
         this.values = [];
         this.iteratorIdx = 0;
+        this[Symbol.iterator] = function () {
+            return new GroupIterator(this);
+        };
     }
 
     add(val) {
@@ -62,10 +65,6 @@ class GroupIterator {
         return {value, done: false}
     }
 }
-
-Group.prototype[Symbol.iterator] = function() {
-    return new GroupIterator(this);
-  };
 
 for (let value of Group.from(["a", "b", "c"])) {
     console.log(value);
